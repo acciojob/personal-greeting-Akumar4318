b90b340097bcd413ed8669ff11048803f162112d
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2025-01-02 12:59:31 +0000

    updated

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -3,13 +3,16 @@ import React, { useState } from "react"
 const Name = () => {
 
     const[name,setName]=useState('')
+    const[hello,setHello]=useState('')
   return (
     <div>
         <h1>Enter your Name</h1>
         <input type="text" onChange={(e)=>{
             setName(e.target.value)
         }} />
-        <p>{name}</p>
+        <p>{
+            name.length==0 ? (''):(`Hello ${name}!`)
+            }</p>
 
     </div>
   )
